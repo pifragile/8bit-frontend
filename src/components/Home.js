a7d799e35dd78c6ffe0b8c45bba1c6a1db6bf936
pifragile <pigu@bluewin.ch>
2022-12-11 12:14:21 +0100

    add images

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,11 +12,25 @@ function Home() {
 
     return (
         <Layout>
+            <div style={{  }}>
+            1byte is a generative audio project by {" "}
+                        <a href="https://twitter.com/pifragile/" target="_blank" rel="noreferrer"> pifragile</a>.
+                All sounds are created using Tone.js and melodies are algorithmically generated. See code snippets below.
+
+            <button style={{width: "min(400px, 80vw)", marginTop: "5vh"}} onClick={handleMint}>Mint for {formatMutez(tokenPrice)}</button>
+            </div>
             <div style={{ marginTop: "10vh" }}>
-            <button style={{width: "min(400px, 80vw)"}} onClick={handleMint}>Mint for {formatMutez(tokenPrice)}</button>
+                <h1>Behind the scenes</h1>
+
+                <img src="img/0.png"></img>
+                <br/>
+                <img src="img/1.png"></img>
+                <br/>
+                <img src="img/2.png"></img>
             </div>
             <div style={{ marginTop: "5vh" }}>
                 <h1>All tokens</h1>
+                Click the token to open the detail view where you can play the sound.
                 <TokenOverview
                     query={`v1/tokens?contract=${contract}`}
                     pageLength={30}
